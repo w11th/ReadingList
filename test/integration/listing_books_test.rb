c8217d4cc444b4e982c0fd0b9822924c662547e2
Wenhui Wang <w.eleventh@gmail.com>
2017-05-17 11:38:07 +0800

    Listing books, filter whith rating

---
+++ test/integration/listing_books_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ListingBooksTest < ActionDispatch::IntegrationTest
+  setup do
+    Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: "Ender's Game", rating: 4)
+  end
+
+  test 'listing books' do
+    get '/books'
+    assert_equal 200, response.status
+    assert_equal Mime[:json], response.content_type
+    assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'listing top rated books' do
+    get '/books?rating=5'
+    assert_equal 200, response.status
+    assert_equal Mime[:json], response.content_type
+    assert_equal 1, JSON.parse(response.body).size
+  end
+end
